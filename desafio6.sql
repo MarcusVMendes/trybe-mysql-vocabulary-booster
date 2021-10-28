e78cf8f0441824a8fe16a2610d2a77f73729526a
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-10-27 21:26:42 -0300

    Requisito 6 concluido

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,10 @@
 SELECT
-CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+CONCAT(E.FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
 J.JOB_TITLE AS `Cargo`,
 JH.START_DATE AS `Data de início do cargo`,
 D.DEPARTMENT_NAME AS `Departamento`
-FROM hr.employees AS E
-JOIN hr.jobs AS J ON J.JOB_ID = E.JOB_ID
-JOIN hr.job_history AS JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
-JOIN hr.departments AS D ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+FROM hr.job_history AS JH
+JOIN hr.employees AS E ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+JOIN hr. jobs AS J ON J.JOB_ID = JH.JOB_ID
+JOIN hr.departments AS D ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, `Cargo` ASC;
